44c195425de62e5895d7c26c241ac6ba80a5aa91
paulsaiwa <saiwavincent@gmail.com>
2022-06-30 08:16:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-
-/**
- *  * main -starting point
- *   *
- *    * Return: Always 0 (Success)
- *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
