9c11c304b896b7d80d25e35c025b2f27b73060cb
paulsaiwa <saiwavincent@gmail.com>
2022-06-30 08:21:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-\**
-* function return 0
-	* output text only
-	*\
+\* displaying text only 	*\
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
