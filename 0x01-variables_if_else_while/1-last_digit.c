1d567e9b1b580b4ec797efd09b47500b03bb2d46
paulsaiwa <saiwavincent@gmail.com>
2022-07-01 04:33:46 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * print randomly generated numbers
  * indentinh is very important
  */
-
 int main(void)
 {
 	int n;
@@ -21,7 +19,7 @@ int main(void)
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
+	}i
 	else 
 	{
 		 printf("Last digit of %d is %d and is 0\n", n, n % 10);
