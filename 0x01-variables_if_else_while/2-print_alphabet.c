6194161b3b4ef1f86b237aeb0251b8d16552b1b8
paulsaiwa <saiwavincent@gmail.com>
2022-07-01 05:02:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * The program prints out all alphabet letter.
+ * 
+ * Return : Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a', letter <= 'z', letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
