505bd50805cab75cb1b9df430ec6451ec8d626f9
paulsaiwa <saiwavincent@gmail.com>
2022-07-01 06:01:59 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@ int main(void)
 	 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'e' && letter <= 'q')
+		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
 	putchar(letter);
