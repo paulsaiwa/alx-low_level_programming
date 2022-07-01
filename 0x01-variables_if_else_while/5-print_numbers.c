658e7a3d2a5ceabd717d5db0b41a7e36e09c995b
paulsaiwa <saiwavincent@gmail.com>
2022-07-01 06:13:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints out single digital numbersof base 10.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+	printf("\n");
+	return (0);
+}
+
