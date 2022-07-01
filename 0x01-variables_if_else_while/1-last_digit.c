f8f31ac82b2d17e8563ae5aeb6de68d3c43139f6
paulsaiwa <saiwavincent@gmail.com>
2022-07-01 04:40:54 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,16 @@
+/**
+ * The program prints out random number
+ */    
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- * print randomly generated numbers
- * indentinh is very important
- */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
