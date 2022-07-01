dda1543f06da408e63b812561d7f5ca06f36f2fb
paulsaiwa <saiwavincent@gmail.com>
2022-07-01 04:29:02 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -25,5 +26,5 @@ int main(void)
 	{
 		 printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	return 0;
+	return (0);
 }
