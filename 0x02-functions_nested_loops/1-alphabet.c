f1f8a894b4b11b21bd2f78f3708ee227239778fc
paulsaiwa <saiwavincent@gmail.com>
2022-07-05 04:21:56 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,27 +2,14 @@
 /**
  * function: prototype for printing lower alphabet character using _putchar function
  */
-void print_alphabet(void);
-/**
- * main - calling and executing void print_alphabet(void); function
- * Return: Always 0.
- */
-int main(void)
-{
-	char letter1;
-	char letter2 = print_alphanet(letter1);
-	_putchar(letter1);
-
-}
-/**
- *Function: void print_alphabet(void) definitio
- *Return:0
- */
 void print_alphabet(void)
 {
-	for (char letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 	return (0);
 }
-
 
